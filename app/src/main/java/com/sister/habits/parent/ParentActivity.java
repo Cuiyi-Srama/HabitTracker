a368088e1df634f64b81a2afb0152ec72ffa0786
Cuiyi-Srama <2062543115@qq.com>
2026-07-26 15:54:30 +0800

    fix: add missing ViewGroup import in ParentActivity

--- app/src/main/java/com/sister/habits/parent/ParentActivity.java
+++ app/src/main/java/com/sister/habits/parent/ParentActivity.java
@@ -2,6 +2,7 @@ package com.sister.habits.parent;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
