9b99b7938e4de7a485d007f5fcc633fd840f142b
Cuiyi-Srama <2062543115@qq.com>
2026-07-26 15:42:53 +0800

    fix: add SwitchCompat import

--- app/src/main/java/com/sister/habits/parent/ParentActivity.java
+++ app/src/main/java/com/sister/habits/parent/ParentActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
