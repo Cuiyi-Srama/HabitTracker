00ee2b53fe03b7136691294c81c0229ca11fa634
Cuiyi-Srama <2062543115@qq.com>
2026-08-13 00:12:40 +0800

    v3.0.58: SupportSQLiteDatabase.query(Object[])修正

--- app/src/main/java/com/sister/habits/sync/LanSync.java
+++ app/src/main/java/com/sister/habits/sync/LanSync.java
@@ -383,7 +383,7 @@ public class LanSync {
         android.database.Cursor c = null;
         try {
             androidx.sqlite.db.SupportSQLiteDatabase sqldb = db.getOpenHelper().getWritableDatabase();
-            c = sqldb.rawQuery("SELECT iconUrl FROM shop_items WHERE iconUrl LIKE ?", new String[]{"%" + fileName});
+            c = sqldb.query("SELECT iconUrl FROM shop_items WHERE iconUrl LIKE ?", new Object[]{"%" + fileName});
             while (c.moveToNext()) {
                 String p = c.getString(0);
                 if (p != null && !p.isEmpty()) {
