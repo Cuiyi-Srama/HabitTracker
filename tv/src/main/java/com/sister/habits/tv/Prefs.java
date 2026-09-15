2757b1a46bb291458c5f22bcb5055b398b54d7d0
Cuiyi-Srama <2062543115@qq.com>
2026-09-15 18:06:43 +0800

    feat(tv): M3 TV版模块 tv/src/main/java/com/sister/habits/tv/Prefs.java

---
+++ tv/src/main/java/com/sister/habits/tv/Prefs.java
@@ -0,0 +1,38 @@
+package com.sister.habits.tv;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/** TV 端配置（服务器地址 + 家庭 Token），可在电视上用遥控器修改 */
+public final class Prefs {
+
+    private static final String FILE = "tv_prefs";
+    public static final String DEFAULT_HUB = "http://192.168.1.16:23458/habit/cuiyi/tv";
+    public static final String DEFAULT_TOKEN = "kDSBygdyGS0gWt0EZbyGei0qRbyAW94O";
+
+    private final SharedPreferences sp;
+
+    public Prefs(Context ctx) {
+        sp = ctx.getApplicationContext().getSharedPreferences(FILE, Context.MODE_PRIVATE);
+    }
+
+    public String hub() {
+        return sp.getString("hub", DEFAULT_HUB);
+    }
+
+    public String token() {
+        return sp.getString("token", DEFAULT_TOKEN);
+    }
+
+    public void setHub(String value) {
+        sp.edit().putString("hub", value).apply();
+    }
+
+    public void setToken(String value) {
+        sp.edit().putString("token", value).apply();
+    }
+
+    public HubApi api() {
+        return new HubApi(hub(), token());
+    }
+}
